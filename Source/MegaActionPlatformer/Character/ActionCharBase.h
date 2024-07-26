8a7474d08920d5cea7ed2c247105865ba76841e7
unreal5 <lingbao@outlook.com>
2024-07-26 15:24:51 +0800

    19-inheritance and makeing the character blueprint

---
+++ Source/MegaActionPlatformer/Character/ActionCharBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperZDCharacter.h"
+#include "ActionCharBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MEGAACTIONPLATFORMER_API AActionCharBase : public APaperZDCharacter
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/MegaActionPlatformer/Character/EnemyBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/ActionCharBase.h"
+#include "EnemyBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MEGAACTIONPLATFORMER_API AEnemyBase : public AActionCharBase
+{
+	GENERATED_BODY()
+	
+};
